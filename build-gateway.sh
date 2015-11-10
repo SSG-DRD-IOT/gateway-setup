669e3f6ddd82f145090121e9d7b9509d4b775a08
vaghesh <vaghesh@gmail.com>
2015-11-10 14:21:15 -0800

    Build Gateway

---
+++ build-gateway.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TGTDEV=/dev/sda
+
+yes | tgt=${TGTDEV} /sbin/reset_media
+sed -e 's/\t\([0-9a-zA-Z]*\)[ \t].*/\1/' << EOF | fdisk ${TGTDEV}
+	n # create a new partition
+	p # primary partition
+	  # press return 
+	  # for default value
+	  # for default value
+	w # quit fdisk
+EOF
+mkfs.ext4 /dev/sda3
+mount /dev/sda2 /mnt
+echo '/dev/sda3	/data	auto	defaults	1	1' >> /mnt/etc/fstab
+mkdir -p /mnt/data/db
+wget -O /mnt/root http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.6.tgz
+tar zxvf -C /mnt/root/ /mnt/root/mongodb-linux-x86_64-3.0.6.tgz
+mv /mnt/root/mongodb-linux-x86_64-3.0.6/bin/* /mnt/usr/bin/
+reboot
