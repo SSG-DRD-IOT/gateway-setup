7109f8494f72dde0d727df9243e0a362e0b98cf8
Daniel Holmlund <agnathan@gmail.com>
2016-05-31 15:12:53 -0700

    Updated the flashing commands for MoonIsland 3.1

--- build-gateway.sh
+++ build-gateway.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 # Use this command to flash the gateway
-# wget -O -  http://bit.ly/iot-gateway | bash 
-TGTDEV=/dev/sda
+# wget -O - http://bit.ly/iot-gateway | bash 
 
-yes | tgt=${TGTDEV} /sbin/reset_media
-sed -e 's/\t\([0-9a-zA-Z]*\)[ \t].*/\1/' << EOF | fdisk ${TGTDEV}
-	n # create a new partition
-	p # primary partition
-	  # press return 
-	  # for default value
-	  # for default value
-	w # quit fdisk
-EOF
-mkfs.ext4 /dev/sda3
-mount /dev/sda2 /mnt
-echo '/dev/sda3	/data	auto	defaults	1	1' >> /mnt/etc/fstab
-mkdir -p /mnt/data/db
-
+flash_gateway () {
+	yes | /sbin/deploytool -d /dev/sda --reset-media -F
+	VOLUME_GROUP_NAME=$(pvscan | egrep sda | cut -d ' ' -f 8)
+	LOGICAL_VOLUME_NAME=$(lvdisplay $VOLUME_GROUP_NAME | egrep "LV Name" | sed 's/\s\s*/ /g'|cut -d ' ' -f 4)
+	MOUNT_PATH=/dev/$VOLUME_GROUP_NAME/$LOGICAL_VOLUME_NAME
+	mount $MOUNT_PATH /mnt
+}
+	
 configure_sftp () {
 	perl -pi -w -e "s/\/usr\/lib64\/openssh\/sftp-server/internal-sftp/g" /etc/ssh/sshd_config
 }
 
 installMongoDB () {
+  mkdir -p /mnt/data/db
   wget --directory-prefix=/mnt/root http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.0.6.tgz
   tar zxvf /mnt/root/mongodb-linux-x86_64-3.0.6.tgz --directory=/mnt/root/
   mv /mnt/root/mongodb-linux-x86_64-3.0.6/bin/* /mnt/usr/bin/
@@ -56,6 +50,7 @@ whiteListing(){
 	paxctl -Cm /mnt/usr/bin/mongo	
 }
 
+flash_gateway
 installMongoDB
 randomizeChannel
 configure_sftp
