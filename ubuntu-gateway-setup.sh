52a02f16739b0181cc92085748ce303889a44798
Huzefa N K <huzefank@gmail.com>
2017-05-05 08:59:43 -0700

    minor correction for docker installation command

--- ubuntu-gateway-setup.sh
+++ ubuntu-gateway-setup.sh
@@ -57,12 +57,12 @@ if [ "$platform" == "$CORE_PLATFORM" ]; then
     apt-get install -y grafana
 
     echo -e "${Y}Install docker and it's dependencies...${NC}\n"
-    apt-get install apt-transport-https
+    apt-get install -y apt-transport-https
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     apt-key fingerprint 0EBFCD88
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     apt-get update
-    apt-get install docker-ce
+    apt-get install -y docker-ce
 fi
 
 echo -e "${Y}Install Node..${NC}\n"
