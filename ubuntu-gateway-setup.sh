be0a9376c22bb7ee9839b020962cb068ca39ff8a
Huzefa N K <huzefank@gmail.com>
2017-05-08 15:11:38 -0700

    add hdc configuration and reorganize the script
    -Add HDC setup files and install instructions
    -Create folders to organize the setup files
    -Re-organize the script contents

--- ubuntu-gateway-setup.sh
+++ ubuntu-gateway-setup.sh
@@ -16,6 +16,77 @@ GATEWAY_DIR="gateway-setup"
 CUR_DIR="${PWD##*/}"
 platform=$(cat /sys/devices/virtual/dmi/id/board_name)
 
+install_docker() {
+    echo -e "${Y}Install docker and it's dependencies...${NC}\n"
+    apt-get install -y apt-transport-https
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    apt-key fingerprint 0EBFCD88
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install -y docker-ce
+}
+
+install_hdc() {
+    echo -e "${Y}Install and setup configuration for Helix Device Cloud (HDC) agent...${NC}\n"
+    apt-get install -y $PWD/conf_files/hdc/hdc-2.3.0-x86_64.deb
+    cp conf_files/hdc/startup.bin /var/lib/iot
+    #configure using the iot-control utility installed by HDC agent
+    iot-control -c /var/lib/iot/startup.bin
+}
+
+install_node() {
+    echo -e "${Y}Install Node..${NC}\n"
+    curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+    apt-get install -y nodejs
+}
+
+install_and_setup_node-red() {
+    echo -e "${Y}Install Node-Red and it's UPM Grove kit npm packages...${NC}\n"
+    npm install -g node-red
+    npm install -g node-red-contrib-upm
+
+    echo -e "${Y}Create & add Node-Red user to dialout group for ttyACM0 access${NC}\n"
+    useradd node-red -G dialout
+    mkdir -p /home/node-red/.node-red
+    chown -R node-red:node-red /home/node-red
+
+    echo -e "${Y}Setup imraa & Node-Red services and default flows...${NC}\n"
+    cp conf_files/node-red/node-red-experience.timer /lib/systemd/system/node-red-experience.timer
+    cp conf_files/node-red/node-red-experience.service /lib/systemd/system/node-red-experience.service
+    cp conf_files/mraa-imraa.service /lib/systemd/system/mraa-imraa.service
+    cp conf_files/node-red/flows_ip.json /home/node-red/.node-red/flows_$HOSTNAME.json
+    cp utils/dfu-util /usr/bin/
+
+    #run daemon-reload for this to take effect
+    systemctl daemon-reload
+
+    #Enable node-red timer which will start the service after a short time on boot
+    systemctl enable node-red-experience.timer
+}
+
+install_mraa_upm_plugins() {
+    echo -e "${Y}Install MRAA, UPM and its dependencies..${NC}\n"
+    add-apt-repository -y ppa:mraa/mraa
+    apt-get update
+    apt-get install -y libmraa1 libmraa-dev mraa-tools mraa-imraa python-mraa \
+        python3-mraa libupm-dev python-upm python3-upm upm-examples
+
+    echo -e "${Y}Install MRAA and UPM plugins for java script...${NC}\n"
+    #Install MRAA & UPM plugins for java script
+    npm install -g mraa
+    #npm install -g upm
+    npm install -g jsupm_grove
+    npm install -g jsupm_i2clcd
+}
+
+install_xdk_daemon() {
+    echo -e "${Y}Download and Install Intel XDK daemon...${NC}\n"
+    wget http://download.xdk.intel.com/iot/xdk-daemon-0100-x64-ubuntu-anyboard-node-4.5.0_master.tar.bz2
+    tar xvf xdk-daemon-0100-x64-ubuntu-anyboard-node-4.5.0_master.tar.bz2
+    cd xdk-daemon-0100-x64-node-4.5.0
+    ./setup.sh
+}
+
 echo -e "${Y}********** Start of Script ***********${NC}\n"
 
 if [[ $EUID -ne 0 ]]; then
@@ -42,7 +113,7 @@ echo -e "${Y}Modify the sshd_config file for ssh access to root user, it is disa
 sed -ie 's/prohibit-password/yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
 
-#Install few packages only required for our labs running with core i7
+#Configuration required only for our labs running core i7
 if [ "$platform" == "$CORE_PLATFORM" ]; then
     echo -e "${Y}Install MongoDB package...${NC}\n"
     apt-get install -y mongodb
@@ -56,52 +127,21 @@ if [ "$platform" == "$CORE_PLATFORM" ]; then
     echo -e "${Y}Install grafana module...${NC}\n"
     apt-get install -y grafana
 
-    echo -e "${Y}Install docker and it's dependencies...${NC}\n"
-    apt-get install -y apt-transport-https
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    apt-key fingerprint 0EBFCD88
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install -y docker-ce
+    #Install and setup docker tool
+    install_docker
+
+    #Install and configure Helix Device Cloud (HDC) agent
+    install_hdc
 fi
 
-echo -e "${Y}Install Node..${NC}\n"
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-apt-get install -y nodejs
+#Install Node
+install_node
 
-echo -e "${Y}Install MRAA, UPM and its dependencies..${NC}\n"
-add-apt-repository -y ppa:mraa/mraa
-apt-get update
-apt-get install -y libmraa1 libmraa-dev mraa-tools mraa-imraa python-mraa python3-mraa libupm-dev python-upm python3-upm upm-examples
+#Install MRAA UPM and plugins for JS
+install_mraa_upm_plugins
 
-echo -e "${Y}Install MRAA and UPM plugins for java script...${NC}\n"
-#Install MRAA & UPM plugins for java script
-npm install -g mraa
-#npm install -g upm
-npm install -g jsupm_grove
-npm install -g jsupm_i2clcd
-
-echo -e "${Y}Install Node-Red and Node-Red UPM Grove kit - npm packages...${NC}\n"
-npm install -g node-red
-npm install -g node-red-contrib-upm
-
-echo -e "${Y}Create Node-Red user and add it to dialout group for access to ttyACM0 device...${NC}\n"
-useradd node-red -G dialout
-mkdir -p /home/node-red/.node-red
-chown -R node-red:node-red /home/node-red
-
-echo -e "${Y}Setup imraa & Node-Red services and default flows...${NC}\n"
-cp node-red-experience.timer /lib/systemd/system/node-red-experience.timer
-cp node-red-experience.service /lib/systemd/system/node-red-experience.service
-cp mraa-imraa.service /lib/systemd/system/mraa-imraa.service
-cp flows_ip.json /home/node-red/.node-red/flows_$HOSTNAME.json
-cp dfu-util /usr/bin/
-
-#run daemon-reload for this to take effect
-systemctl daemon-reload
-
-#Enable and start the node red service
-systemctl enable node-red-experience.timer
+#Install and configure node-red module
+install_and_setup_node-red
 
 echo -e "${Y}Export node path(NODE_PATH) by adding it to bashrc file...${NC}\n"
 echo 'export NODE_PATH=/usr/lib/node_modules/' >> ~/.bashrc
@@ -109,11 +149,8 @@ echo 'export NODE_PATH=/usr/lib/node_modules/' >> ~/.bashrc
 #script exits
 source ~/.bashrc
 
-echo -e "${Y}Download and Install Intel XDK daemon, it will be used to connect with Intel XDK...${NC}\n"
-wget http://download.xdk.intel.com/iot/xdk-daemon-0100-x64-ubuntu-anyboard-node-4.5.0_master.tar.bz2
-tar xvf xdk-daemon-0100-x64-ubuntu-anyboard-node-4.5.0_master.tar.bz2
-cd xdk-daemon-0100-x64-node-4.5.0
-./setup.sh
+#Download and install XDK daemon to connect to XDK
+install_xdk_daemon
 
 echo -e "\n${Y}********** End of Script ***********${NC}\n"
 echo -e "${Y}********** Rebooting after installation **********${NC}\n"
