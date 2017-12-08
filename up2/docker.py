a8fffc4e29f000076a5f925db57447812ab5d6b9
Daniel Holmlund <agnathan@gmail.com>
2017-12-08 12:36:24 -0800

    added check to see if docker is installed

--- up2/docker.py
+++ up2/docker.py
@@ -7,16 +7,17 @@ def docker():
 
     # The GPG keyid of the Docker repository
     repository_key_id = "39B88DE4"
+    if not files.exists("/usr/bin/docker"):
+        with settings(warn_only=True):
+            apt_install("apt-transport-https")
+            apt_install("curl")
 
-    apt_install("apt-transport-https")
-    apt_install("curl")
-
-    result = repository_key_check(repository_key_id)
-    if result == False:
-        sudo("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
-        sudo("add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable'")
-        sudo("apt-get update")
-        apt_install("docker-ce")
+            result = repository_key_check(repository_key_id)
+            if result == False:
+                sudo("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+                sudo("add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable'")
+                sudo("apt-get update")
+                apt_install("docker-ce")
 
 @task
 def install():
--- up2/fabfile.py
+++ up2/fabfile.py
@@ -5,13 +5,12 @@ import atom
 import docker
 
 env.hosts = [
-    '192.168.3.44'
-    # '192.168.3.46'
+    # '192.168.3.44'
+    '192.168.3.46'
 ]
 
 env.user = "upsquared"
 env.password = "upsquared"
-
 
 @task
 def whoami():
